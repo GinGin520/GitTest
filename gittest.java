932600cd71193bba593be9f83a92dcb03f8ac88c
ginhe <jhe44@u.rochester.edu>
2023-09-14 11:49:16 -0400

    stage 1

--- gittest.java
+++ gittest.java
@@ -11,6 +11,8 @@ public class gittest {
         System.out.println("Hello World3");
 
         System.out.println("Test branch");
+
+        System.out.println("Test branch2, stage 1");
     }
 }
 
